493a9ed5db3ae99f3db70820f178d651bed7cf21
samratchoudhury <73993794+samratchoudhury@users.noreply.github.com>
2021-05-28 23:10:45 +0530

    Add files via upload

---
+++ variabe-and-datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 15;
+	int b = 22;
+	int c = a + b;
+	cout<<c <<endl;
+
+
+char d ='x';
+cout<<d <<endl;
+
+float f=1.23;
+cout<<f<<endl;
+
+bool b1=true;
+
+int size = sizeof(f);
+cout<<size<<endl;
+
+}
